bef9898bb976ca9309745b1424799dbac0ea1fb8
rata854 <nitiham2006@gmail.com>
2021-06-07 03:06:06 +0000

    [Finish]bookers7b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,24 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
+    
+    @today = @books.created_today.count
+    @yesterday = @books.created_yesterday.count
+    if @today == 0 || @yesterday == 0
+      @day_before_ratio = 0
+    else
+      @day_before_ratio = @today / @yesterday * 100
+    end
+    
+    @this_week = @books.created_this_week.count
+    @last_week = @books.created_last_week.count
+    if @this_week == 0 || @last_week == 0
+      @week_over_week = 0
+    else
+      @week_over_week = @this_week / @last_week * 100
+    end
   end
-
+  
   def index
     @users = User.all
     @book = Book.new
@@ -23,7 +39,7 @@ class UsersController < ApplicationController
       render "edit"
     end
   end
-
+  
   private
 
   def user_params
--- app/models/book.rb
+++ app/models/book.rb
@@ -9,5 +9,9 @@ class Book < ApplicationRecord
 		favorites.where(user_id: user.id).exists?
 	end
 	
-	scope :current_month, -> { where(created_at: Time.now.beginning_of_month..Time.now.end_of_month) }
+	scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+	scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+	scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
+	scope :created_last_week, -> { where(created_at: 13.day.ago.beginning_of_day..7.day.ago.end_of_month) }
+
 end
